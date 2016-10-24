464682516786ac4f8913d8632936a865598ad47f
rkalvani <student@rkalvani.local>
2016-10-24 09:57:33 -0500

    Initial Commit

---
+++ Psych Episode DatabaseTests/Psych_Episode_DatabaseTests.swift
@@ -0,0 +1,36 @@
+//
+//  Psych_Episode_DatabaseTests.swift
+//  Psych Episode DatabaseTests
+//
+//  Created by rkalvani on 10/24/16.
+//  Copyright © 2016 rkalvani. All rights reserved.
+//
+
+import XCTest
+@testable import Psych_Episode_Database
+
+class Psych_Episode_DatabaseTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
